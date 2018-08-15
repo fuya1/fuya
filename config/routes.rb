54752ab47dab727721eff67b1d9a6258eb8266ea
SimDaSong <song98987@korea.ac.kr>
2018-08-15 08:05:00 +0000

    delete rails_admin gem[dasong]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :shop_reviews
   resources :market_reviews
