e720a1ee36cf136792c252971eef2472349a5270
SimDaSong <song98987@korea.ac.kr>
2018-08-15 08:36:15 +0000

    update[dasong]

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :market_reviews
+  has_many :shop_reviews
+end
